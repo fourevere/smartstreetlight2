1ea1697bfc39bd3790fe626e651bd89ec29b57af
fourevere <jgk8866@naver.com>
2021-07-19 12:38:32 +0900

    0.9

---
+++ app/src/main/java/com/jung/smartstreetlight/MainActivity.java
@@ -0,0 +1,157 @@
+package com.jung.smartstreetlight;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //위치권한설정
+    int nCurrentPermission = 0;
+    static final int PERMISSIONS_REQUEST = 0x0000001;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        키해시 불러오는 코드
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+*/
+
+        //위치권한설정
+        OnCheckPermission();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(
+                            "back_id",
+                            "백그라운드",
+                            NotificationManager.IMPORTANCE_DEFAULT
+                    );
+            notificationChannel.setDescription("백그라운드");
+            notificationManager.createNotificationChannel(notificationChannel);
+
+     /*       NotificationChannel notificationChannel2 =
+                    new NotificationChannel(
+                            "push_message",
+                            "푸쉬알림",
+                            NotificationManager.IMPORTANCE_HIGH
+                    );
+            notificationChannel2.setDescription("푸쉬알림");
+            notificationManager.createNotificationChannel(notificationChannel2); */
+
+            String NOTIFICATION_ID = "push_message";
+            String NOTIFICATION_NAME = "푸쉬알림";
+            int IMPORTANCE = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel2 = new NotificationChannel(NOTIFICATION_ID, NOTIFICATION_NAME, IMPORTANCE);
+            notificationManager.createNotificationChannel(channel2);
+
+
+        }
+
+
+        ImageButton navi = (ImageButton) findViewById(R.id.navi);
+
+
+        ImageButton qna = (ImageButton) findViewById(R.id.qna);
+
+        qna.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Qna.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton notice = (ImageButton) findViewById(R.id.notice);
+
+        notice.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Notice.class);
+                Intent intent2 = new Intent(getApplicationContext(), backgroundservice.class);
+                startActivity(intent);
+                startService(intent2);
+            }
+        });
+
+        ImageButton info = (ImageButton) findViewById(R.id.info);
+
+        info.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Info.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    //위치권한설정
+    public void OnCheckPermission() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Toast.makeText(this, "위치권한설정중..",Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST :
+                if (grantResults.length > 0
+                        &&grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "설정완료", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "이앱을 사용하기 위해선 위치권한 설정이 필요합니다", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/jung/smartstreetlight/backgroundservice.java
@@ -0,0 +1,219 @@
+package com.jung.smartstreetlight;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+
+public class backgroundservice extends Service {
+
+    //public static final String MESSEAGE_KEY ="";
+
+
+
+  //  int stop = 0;
+
+//
+    private final Handler mHandler = new Handler();
+    String rcvIp, rcvPort, rcvPacket;
+
+
+    private final Runnable rnb = new Runnable() {
+        @Override
+        public void run() {
+
+        //    if(stop == 1) {
+
+                NotificationCompat.Builder mBuilder2 =
+                        new NotificationCompat.Builder(backgroundservice.this, "push_message")
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setSmallIcon(R.drawable.ic_stat_image2)
+                                .setContentTitle("스마트가로등시스템")
+                                .setContentText("가로등에서 보행자가 감지되었습니다")
+                                .setTimeoutAfter(3000)
+                                .setColor(ContextCompat.getColor(backgroundservice.this, R.color.teal_700));
+                //   //                 .setAutoCancel(true)
+                NotificationManager mNotifyMgr2 =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                mNotifyMgr2.notify(002, mBuilder2.build());
+                mp.start();
+       //         stop = 0;
+      //      }
+        }
+    };
+    //
+
+
+
+
+    public backgroundservice() {
+    }
+
+    MediaPlayer mp;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        //서비스에서 가장 먼저 호출(최초한번)
+        mp = MediaPlayer.create(this, R.raw.ssyour);
+        mp.setLooping(false); // 반복재생
+        super.onCreate();
+
+//
+        ReceiveData rcvServer = new ReceiveData(8266);
+        rcvServer.start();
+
+
+//
+    }
+
+
+    //
+    class ReceiveData extends Thread {
+        //UDP로 패킷을 받았을때 받은 패킷을 UI로 올리기 위해 handler 받아옴
+        Handler handler = mHandler;
+        DatagramSocket rSocket;
+
+        //UDP 수신 포트를 열기(원하는 port 아무거나)
+        public ReceiveData(int port) {
+            try {
+                //수신 소켓 생성
+                rSocket = new DatagramSocket(port);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            try {
+                while (true) {
+              //      stop = 1;
+                    //받을 패킷 생성
+                    byte[] buf = new byte[1024];
+                    //패킷으로 변경 (바이트 버퍼, 버퍼길이)
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    //데이터 수신 대기
+                    rSocket.receive(packet);
+                    //패킷을 보낸 상대방의 ip를 저장함
+                    InetAddress ina = packet.getAddress();
+                    rcvIp = ina.toString();
+                    //패킷을 보낸 상대방의 port를 저장함
+                    int inp = packet.getPort();
+                    rcvPort = String.valueOf(inp);
+                    //수신받은 데이터를 문자열로 변환
+                    rcvPacket = new String(buf);
+                    //에코하기 위해 송신용 패킷을 만듦 (받은 패킷, 패킷 길이, 상대방 IP, 상대방 PORT)
+                    packet = new DatagramPacket(buf, buf.length, ina, inp);
+                    //이 기기로 UDP송신을 한 상대방에게 받은패킷을 돌려줌
+                    rSocket.send(packet);
+                    //데이터를 받았다면 UI로 표현해주기 위해 runnable 사용
+                    handler.post(rnb);
+                    //쓰레드를 인터럽트로 종료시키기 위해 sleep을 사용함
+                    sleep(20);
+                }
+            } catch (InterruptedException e) {
+                rcvPacket = e.toString();
+                handler.post(rnb);
+            } catch (Exception e) {
+                rcvPacket = e.toString();
+                handler.post(rnb);
+            }
+        }
+
+    }
+//
+
+
+
+
+
+    @Override
+    public int onStartCommand(Intent intent,int flags, int startId) {
+
+        //   boolean message = intent.getExtras().getBoolean(backgroundservice.MESSEAGE_KEY);
+
+      //  int name = intent.getExtras().getInt("name");
+
+      //      if(name == 1) {
+
+
+            Intent mMainIntent = new Intent(this,MainActivity.class);
+            PendingIntent mPendingIntent = PendingIntent.getActivity(
+                    this,1,mMainIntent,PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this,"back_id")
+                            .setSmallIcon(R.drawable.ic_stat_image2)
+                            .setContentTitle("스마트가로등시스템")
+                            .setContentIntent(mPendingIntent)
+                            .setOngoing(true)
+                            .setContentText("현재 주위 보행자알림서비스가 실행중입니다");
+            NotificationManager mNotifyMgr =
+                    (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            mNotifyMgr.notify(001, mBuilder.build());
+
+
+
+
+
+
+/*            if(rcvPacket !=null && !rcvPacket.equals("")) {
+                mp.start();
+                NotificationCompat.Builder mBuilder2 =
+                        new NotificationCompat.Builder(this, "push_message")
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setSmallIcon(R.drawable.ic_stat_image2)
+                                .setContentTitle("스마트가로등시스템")
+                                .setContentText("가로등에서 보행자가 감지되었습니다")
+                                .setTimeoutAfter(5000)
+                                .setColor(ContextCompat.getColor(this, R.color.teal_700));
+                //   //                 .setAutoCancel(true)
+                NotificationManager mNotifyMgr2 =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                mNotifyMgr2.notify(002, mBuilder2.build());
+
+
+            }*/
+
+
+
+       //    }
+       //    else {
+       //        mp.stop();
+       //        mp.release();
+
+       //     }
+            return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.stop();
+        mp.release();
+        NotificationManager mNotifyMgr =
+                (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.cancelAll();
+   //     stop = 0;
+        super.onDestroy();
+    }
+}
